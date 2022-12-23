a35c4a30390ce23cffe1a2b07068ca16acf411ad
JGarima2843 <jgarima284@gmail.com>
2022-12-23 10:15:30 +0530

    final architecture

--- controller/QuestionsController.js
+++ controller/QuestionsController.js
@@ -25,6 +25,7 @@ module.exports.showDetails=async function(req,res){
         if(ques){
             res.send(ques);
         }
+        // handling the bad requests if that id does not exist
         else{
             res.send("id does not exits");
         }
@@ -41,8 +42,13 @@ module.exports.deleteQues=async function(req,res){
         if(ques){
             // delete all the option ⁉️ of the option db having the question id as the req.params.id
             await Question.deleteOne(req.params.id).clone().catch(function(err){ console.log(err)})
+            // deleting all the option of that question
             await Option.deleteMany({question:req.params.id}).clone().catch(function(err){ console.log(err)})
                 res.send("ques deleted");
     
+        }
+        //  if th at question of the given id does not exists then just sending a message
+        else{
+            res.send('question does not exists')
         }
 }
